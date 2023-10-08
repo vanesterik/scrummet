e4bce88b795edcc2065f9033c0d31365e585c74b
Koen van Esterik <koen@vanesterik.com>
2023-10-08 13:32:19 +0200

    ci: implement ci cd pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/scrummet/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
