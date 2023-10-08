a47c3b65b772a111b65f9007184e8a217455a87e
Koen van Esterik <koen@vanesterik.com>
2023-10-08 08:51:54 +0200

    fix: adjust base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/scrummet/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
